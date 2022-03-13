0340bd69df5b0637f6e4228bf2d31f8c446b00a3
Irfan858 <irfankurniawan858@gmail.com>
2022-03-13 08:31:04 +0700

    menyelesaikan fitur long-snake

--- snake-game/script.js
+++ snake-game/script.js
@@ -1,5 +1,4 @@
 const CELL_SIZE = 20;
-// Soal no 1: Set canvas size menjadi 600
 const CANVAS_SIZE = 600;
 const REDRAW_INTERVAL = 50;
 const WIDTH = CANVAS_SIZE / CELL_SIZE;
@@ -21,16 +20,32 @@ function initPosition() {
     }
 }
 
+function initHeadAndBody()
+{
+    let head = initPosition();
+    let body = [{x: head.x, y: head.y}];
+
+    return {
+        head: head,
+        body: body,
+    }
+}
+
 function initDirection() {
     return Math.floor(Math.random() * 4);
 }
 
-let snake1 = {
-    color: "purple",
-    position: initPosition(),
+function initSnake(color)
+{
+    return {
+    color: color,
+    ...initHeadAndBody(),
     direction: initDirection(),
     score: 0,
+    }
 }
+
+let snake1 = initSnake("purple");
 
 
 let apple1 = {
@@ -72,10 +87,16 @@ function draw() {
         let ctx = snakeCanvas.getContext("2d");
 
         ctx.clearRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
-        
-        drawCell(ctx, snake1.position.x, snake1.position.y, snake1.color);
 
-        // Soal no 4: draw more apples
+        drawCell(ctx, snake1.head.x, snake1.head.y, snake1.color);
+
+        for (let i = 1; i < snake1.body.length; i++)
+        {
+            drawCell(ctx, snake1.body[i].x, snake1.body[i].y, snake1.color);
+        }
+
+        //drawCell(ctx, snake1.position.x, snake1.position.y, snake1.color);
+
         let img = document.getElementById("apple");
         ctx.drawImage(img, apple1.position.x * CELL_SIZE, apple1.position.y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
         ctx.drawImage(img, apple2.position.x * CELL_SIZE, apple2.position.y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
@@ -88,52 +109,53 @@ function draw() {
 }
 
 function teleport(snake) {
-    if (snake.position.x < 0) {
-        snake.position.x = CANVAS_SIZE / CELL_SIZE - 1;
+    if (snake.head.x < 0) {
+        snake.head.x = CANVAS_SIZE / CELL_SIZE - 1;
     }
-    if (snake.position.x >= WIDTH) {
-        snake.position.x = 0;
+    if (snake.head.x >= WIDTH) {
+        snake.head.x = 0;
     }
-    if (snake.position.y < 0) {
-        snake.position.y = CANVAS_SIZE / CELL_SIZE - 1;
+    if (snake.head.y < 0) {
+        snake.head.y = CANVAS_SIZE / CELL_SIZE - 1;
     }
-    if (snake.position.y >= HEIGHT) {
-        snake.position.y = 0;
+    if (snake.head.y >= HEIGHT) {
+        snake.head.y = 0;
     }
 }
 
-// Soal no 4: tambah apple2 parameter
 function eat(snake, apple1, apple2) {
-    if (snake.position.x == apple1.position.x && snake.position.y == apple1.position.y) {
+    if (snake.head.x == apple1.position.x && snake.head.y == apple1.position.y) {
         apple1.position = initPosition();
         snake.score++;
+        snake.body.push({x: snake.head.x, y: snake.head.y});
     }
-    if (snake.position.x == apple2.position.x && snake.position.y == apple2.position.y) {
+    if (snake.head.x == apple2.position.x && snake.head.y == apple2.position.y) {
         apple2.position = initPosition();
         snake.score++;
+        snake.body.push({x: snake.head.x, y: snake.head.y});
     }
 }
 
 function moveLeft(snake) {
-    snake.position.x--;
+    snake.head.x--;
     teleport(snake);
     eat(snake, apple1, apple2);
 }
 
 function moveRight(snake) {
-    snake.position.x++;
+    snake.head.x++;
     teleport(snake);
     eat(snake, apple1, apple2);
 }
 
 function moveDown(snake) {
-    snake.position.y++;
+    snake.head.y++;
     teleport(snake);
     eat(snake, apple1, apple2);
 }
 
 function moveUp(snake) {
-    snake.position.y--;
+    snake.head.y--;
     teleport(snake);
     eat(snake, apple1, apple2);
 }
@@ -153,10 +175,32 @@ function move(snake) {
             moveUp(snake);
             break;
     }
+    moveBody(snake);
     setTimeout(function() {
         move(snake);
     }, MOVE_INTERVAL);
 }
+
+function moveBody(snake)
+{
+    snake.body.unshift({x: snake.head.x, y: snake.head.y});
+    snake.body.pop();
+}
+
+// function turn(snake, direction)
+// {
+//     const oppositeDirections = {
+//         [DIRECTION.LEFT]: DIRECTION.LEFT,
+//         [DIRECTION.RIGHT]: DIRECTION.RIGHT,
+//         [DIRECTION.UP]: DIRECTION.UP,
+//         [DIRECTION.DOWN]: DIRECTION.DOWN,
+//     }
+
+//     if (direction !== oppositeDirections[snake.direction])
+//     {
+//         snake.direction = direction;
+//     }
+// }
 
 document.addEventListener("keydown", function (event) {
     if (event.key === "ArrowLeft") {
@@ -171,4 +215,10 @@ document.addEventListener("keydown", function (event) {
 
 })
 
-move(snake1);
+function initGame()
+{
+    move(snake1);
+}
+
+initGame();
+
